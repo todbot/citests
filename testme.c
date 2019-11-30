96216e6cd08608a17afd491f78fe9ed5ccf5addb
Tod E. Kurt <tod@todbot.com>
2019-11-30 13:05:00 -0800

    test0

---
+++ testme.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <stdarg.h>
+#include <ctype.h>
+
+
+int main(int argc, char *argv[])
+{
+  printf("\n\nhello there\n\n");
+
+}
